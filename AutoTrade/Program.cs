8acfb8d877fbac867449143d2ef24184407fa618
li zhengquan <entergl@163.com>
2019-03-24 12:26:03 +0800

    提交diam

---
+++ AutoTrade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTrade
+{
+    class Program
+    {
+        static Dictionary<string, DateTime> nextDate = new Dictionary<string, DateTime>();
+
+        static Dictionary<string, string> instrument = new Dictionary<string, string>();
+
+        static void Main(string[] args)
+        {
+            // 初始化币种
+            instrument.Add("eth", "xrp");
+
+            // 获取行情，
+            // 读取数据库 看看以前的交易
+            // 判断是否交易。
+            // 标记下一次读取时间
+        }
+    }
+}
---
+++ DataDao/BaseDao.cs
@@ -0,0 +1,48 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDao
+{
+    public class BaseDao
+    {
+        protected ILog logger = LogManager.GetLogger(typeof(BaseDao));
+
+        public string LikeStr(string str)
+        {
+            StringBuilder sb = new StringBuilder(str);
+            sb.Replace("'", "''");
+            sb.Insert(0, "%", 1);
+            sb.Append("%");
+            sb.Replace(@"\", @"\\");
+
+            return sb.ToString();
+        }
+
+        protected IDapperConnection Database { get; private set; }
+        public BaseDao()
+        {
+            string connectionString = AccountConfigUtils.sqlConfig;
+            var connection = new MySqlConnection(connectionString);
+            Database = new DapperConnection(connection);
+        }
+
+        public string GetStateStringIn(List<string> stateList)
+        {
+            // List<string> stateList = new List<string>() { StateConst.PartialCanceled, StateConst.Filled };
+            var states = "";
+            stateList.ForEach(it =>
+            {
+                if (states != "")
+                {
+                    states += ",";
+                }
+                states += $"'{it}'";
+            });
+            return states;
+        }
+    }
+}
