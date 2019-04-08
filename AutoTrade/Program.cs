92d4e7be62d9bb8bea4385d359f8a962facfc8ae
174474866@qq.com <174474866@qq.com>
2019-04-08 17:32:19 +0800

    1

--- AutoTrade/Program.cs
+++ AutoTrade/Program.cs
@@ -133,7 +133,7 @@ namespace AutoTrade
                     Console.WriteLine($"PrepareBuy --> {quote}, {symbol}");
                     if (oldData.Count > 0)
                     {
-                        logger.Error($"相差间隔 lastPrice: {oldData[0].BuyTradePrice}, nowPrice:{nowPrice}, rate: {nowPrice / oldData[0].BuyTradePrice} --  { oldData[0].BuyTradePrice / nowPrice}");
+                        logger.Error($"相差间隔 lastPrice: {oldData[0].Id} -- {oldData[0].BuyTradePrice}, nowPrice:{nowPrice}, rate: {(oldData[0].BuyTradePrice == 0? 0: (nowPrice / oldData[0].BuyTradePrice))} --  { oldData[0].BuyTradePrice / nowPrice}");
                     }
                     PrepareBuy(quote, symbol, nowPrice);
                     return;
